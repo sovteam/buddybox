d8591aa4cc67e32d630dc37206e9802ca6eb8b7c
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-06-26 17:25:36 -0300

    Sov API WIP

--- app/src/main/java/sov/Sov.java
+++ app/src/main/java/sov/Sov.java
@@ -5,21 +5,22 @@ import java.io.File;
 public class Sov {
 
     interface Listener {
-        void onSovInstall();
         void onMessageReceived(long fromContactId, String type, Object payload);
         void onFileReceived(long fromContactId, Hash hash, File written);
     }
     void initListener(Listener listener) {}
 
-    boolean isInstalled() { return false; }
-    /** @param feature will complete the phrase "To _ you need to install Sov." */
-    void install(String feature) {}
-
-    static long[] contactIds() { return null; }
-
-    static void sendMessage(long toContactId, String type, Object payload, int priority, long timeoutMillis) {};
-
     static Hash serve(File file) { return null; }
     void requestFile(Hash hash, long fromContactId, File toWrite) {}
+
+    /** Launches an Android sharing Intent with a Sov link.
+     * @param title Used for sharing via email, for example.
+     * @param description The message body.
+     * @param payload App specific payload to be serialized. Use native Android types and collections. */
+    void share(String title, String description, Object payload) {}
+
+// Mesh sharing:
+//    static long[] contactIds() { return null; }
+//    static void sendMessage(long toContactId, String type, Object payload, int priority, long timeoutMillis) {};
 
 }
