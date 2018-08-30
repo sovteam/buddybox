75cec0a28bdcaaaba13aa510e8082e075051c249
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-08-30 17:54:31 -0300

    Sov API design brainstorming.

--- app/src/main/java/sov/Sov.java
+++ app/src/main/java/sov/Sov.java
@@ -4,23 +4,32 @@ import java.io.File;
 
 public class Sov {
 
-    interface Listener {
-        void onMessageReceived(long fromContactId, String type, Object payload);
-        void onFileReceived(long fromContactId, Hash hash, File written);
+    class Download {
+        long fromContactId;
+        File file;
     }
-    void initListener(Listener listener) {}
 
-    static Hash serve(File file) { return null; }
-    void requestFile(Hash hash, long fromContactId, File toWrite) {}
+    interface DownloadListener {
+        void onStarted(Download download);
+        void onProgress(Download download, double percentage);
+        void onCompleted(Download download);
+        void onAborted(Download download);
+    }
+    /** Called typically from Application.onCreate() */
+    void initDownloadListener(DownloadListener listener) {}
+
+    void abort(Download download) {};
 
     /** Launches an Android sharing Intent with a Sov link.
-     * @param title Used for sharing via email, for example.
-     * @param description The message body.
-     * @param payload App specific payload to be serialized. Use native Android types and collections. */
-    void share(String title, String description, Object payload) {}
+     * @param subject Used for sharing via email, for example.
+     * @param description The message body. */
+    void shareFile(String subject, String description, File toSend) {}
+
 
 // Mesh sharing:
 //    static long[] contactIds() { return null; }
 //    static void sendMessage(long toContactId, String type, Object payload, int priority, long timeoutMillis) {};
+//    static Hash serve(File file) { return null; }
+//    void requestFile(Hash hash, long fromContactId, File toWrite) {}
 
 }
