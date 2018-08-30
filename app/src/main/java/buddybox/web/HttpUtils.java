910eb04033618d6a4783efcdc7f303b4e052140f
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2018-08-30 19:58:28 -0300

    Minor

--- app/src/main/java/buddybox/web/HttpUtils.java
+++ app/src/main/java/buddybox/web/HttpUtils.java
@@ -11,12 +11,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
